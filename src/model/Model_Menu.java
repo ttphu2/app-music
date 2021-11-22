c6862e9369a3983d20cc51250721aa7969671bae
ttphu2 <ttphu34@gmail.com>
2021-11-22 18:13:47 +0700

    init project

---
+++ src/model/Model_Menu.java
@@ -0,0 +1,43 @@
+
+package model;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+
+public class Model_Menu {
+    String menuName;
+    String icon;
+
+    public Model_Menu() {
+    }
+    
+
+    public Model_Menu(String menuName, String icon) {
+        this.menuName = menuName;
+        this.icon = icon;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public void setMenuName(String menuName) {
+        this.menuName = menuName;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+    public Icon toIcon(){
+        System.out.println(getClass().getResource("/icon/" + icon + ".png"));
+        return new ImageIcon(getClass().getResource("/icon/" + icon + ".png"));
+    }
+    public Icon toIconSelected(){
+        return new ImageIcon(getClass().getResource("/icon/"+icon +"_selected.png"));
+    }
+}
